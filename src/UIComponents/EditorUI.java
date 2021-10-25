5967123460f241e9e97ca4981815ebe7ad173276
Madeline Kahn <mk-acct@outlook.com>
2021-10-24 21:24:07 -0400

    Make save feature work

--- src/UIComponents/EditorUI.java
+++ src/UIComponents/EditorUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -26,21 +27,26 @@ public class EditorUI extends JFrame {
 	 */
 	private class EditorOptions extends JPanel {
 		EditorOptions() {
-			JButton save = new JButton("Save");
-			save.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					FileUtil.saveIndiv(EditorUI.this.saveFile, EditorUI.this.indiv);
-				}
-			});
-			this.add(save);
-			
 			JButton mutate = new JButton("Mutate");
 			mutate.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {indiv.mutate(EditorUI.this.mutationRate);}
 			});
 			this.add(mutate);
+
+			JButton save = new JButton("Save");
+			save.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {EditorUI.this.save();}
+			});
+			this.add(save);
+
+			JButton saveAs = new JButton("Save as...");
+			saveAs.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {EditorUI.this.saveAs();}
+			});
+			this.add(saveAs);
 		}
 	}
 
@@ -76,10 +82,41 @@ public class EditorUI extends JFrame {
 	 * Called by constructors after initializing properties
 	 */
 	private void setup() {
-		this.setTitle((saveFile != null) ? saveFile.getName() : "Individual");
+		this.updateWindowTitle();
 		this.add(new ChromosomeEditor(indiv), BorderLayout.CENTER);
 		this.add(new EditorOptions(), BorderLayout.SOUTH);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	/**
+	 * Update the window title in accordance with the file name
+	 */
+	private void updateWindowTitle() {
+		this.setTitle((saveFile != null) ? saveFile.getName() : "Individual");
+	}
+
+	/**
+	 * Save, or if there is no file yet, save as
+	 */
+	private void save() {
+		if (this.saveFile != null) {
+			FileUtil.saveIndiv(this.saveFile, this.indiv);
+		} else {
+			this.saveAs();
+		}
+	}
+
+	/**
+	 * Save as (show save dialog)
+	 */
+	private void saveAs() {
+		JFileChooser saver = new JFileChooser();
+		int returnVal = saver.showSaveDialog(this);
+		if (returnVal != JFileChooser.APPROVE_OPTION) {return;}
+		File file = saver.getSelectedFile();
+		this.saveFile = file;
+		this.updateWindowTitle();
+		this.save();
 	}
 }
