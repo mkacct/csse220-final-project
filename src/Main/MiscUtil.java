817e9354eb52ee183f56b0c9d1088a6146423842
Sarah Collins <secollins9385@gmail.com>
2021-10-24 22:13:02 -0400

    Added javadocs to MiscUtil

--- src/Main/MiscUtil.java
+++ src/Main/MiscUtil.java
@@ -2,37 +2,55 @@ package Main;
 
 public class MiscUtil {
 
+	/**
+	 * Returns the index of char toFind in arr. If arr does not contain toFind,
+	 * returns -1.
+	 * 
+	 * @param arr    <br>
+	 *               Requires: arr is not empty
+	 * @param toFind
+	 * @return
+	 */
 	public static int arrayIndexOf(char[] arr, char toFind) {
-		for(int i = 0; i<arr.length; i++) {
-			if(arr[i] == toFind) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == toFind) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
+	/**
+	 * Parses a double proportion from a string of the form (double)%, (int)/(int),
+	 * or (double). If the string is not of these forms, returns -1. If the
+	 * proportion is greater than 1, returns -2.
+	 * 
+	 * @param proportion
+	 * @return
+	 */
 	public static double parseProportion(String proportion) {
 		double prop;
-		try{
+		try {
 			prop = Double.parseDouble(proportion);
-		}catch(NumberFormatException err) {
-			if(proportion.indexOf('/')!= -1) {
+		} catch (NumberFormatException err) {
+			if (proportion.indexOf('/') != -1) {
 				try {
-					prop = (double) (Integer.parseInt(proportion.substring(0, proportion.indexOf('/')))/Integer.parseInt(proportion.substring(proportion.indexOf('/')+1)));
-				}catch(NumberFormatException err2) {
+					prop = (double) (Integer.parseInt(proportion.substring(0, proportion.indexOf('/')))
+							/ Integer.parseInt(proportion.substring(proportion.indexOf('/') + 1)));
+				} catch (NumberFormatException err2) {
 					return -1;
 				}
-			} else if(proportion.indexOf('%')!=-1) {
+			} else if (proportion.indexOf('%') != -1) {
 				try {
-					prop = (double)(Double.parseDouble(proportion.substring(0, proportion.indexOf('%')))/100);
-				}catch(NumberFormatException err3) {
+					prop = (double) (Double.parseDouble(proportion.substring(0, proportion.indexOf('%'))) / 100);
+				} catch (NumberFormatException err3) {
 					return -1;
 				}
 			} else {
 				return -1;
 			}
 		}
-		if(prop >1) {
+		if (prop > 1) {
 			return -2;
 		}
 		return prop;
