5fa94796f56d77803eecccafdfe7d349173fbf54
Madeline Kahn <mk-acct@outlook.com>
2021-11-07 13:21:31 -0500

    Document FitnessFunctionEitherExtreme

--- src/SimComponents/FitnessFunctionEitherExtreme.java
+++ src/SimComponents/FitnessFunctionEitherExtreme.java
@@ -1,6 +1,13 @@
 package SimComponents;
 
+/**
+ * Fitness function in which the most fit indivs have either the most or least 1s
+ * @author R_002
+ */
 public class FitnessFunctionEitherExtreme implements FitnessFunction {
+	/**
+	 * Returns the percentage by which either ones or not ones are more frequent than the other
+	 */
 	@Override
 	public int calcFitness(Individual indiv) {
 		char[] chromosome = indiv.getChromosome();
